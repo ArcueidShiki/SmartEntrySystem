6ae721ccc18bee528c05272ceae3ffc2e8aecf71
phoever23 <gaoming93@foxmail.com>
2024-09-16 19:21:03 +0800

    Completed the basic structure of dashboard and add the instruction in README.md

---
+++ cli/src/components/dashboard/PageContent.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './pageContent.css'
+import mask from '../../images/Mask-Detection.png'
+import { useEffect, useState } from 'react';
+
+
+function PageContent() {
+  return (
+    <div className="dashboard">
+      <div className="upper-section">
+        <h1>Display real time image/video</h1>
+        <img src={mask} alt="" />
+      </div>
+
+      <div className="lower-section">
+        <div className="text-box"><h2>Temperature: </h2></div>
+        <div className="text-box"><h2>Mask: </h2></div>
+      </div>
+    </div>
+  )
+}
+
+export default PageContent
