4ae27758eeb0f5f3dbf2f678715a911e0c41359c
phoever23 <gaoming93@foxmail.com>
2024-09-16 19:21:03 +0800

    Completed the basic structure of dashboard and add the instruction in README.md

---
+++ cli/src/App.js
@@ -0,0 +1,23 @@
+// import icons
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'remixicon/fonts/remixicon.css';
+
+// import Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import './App.css';
+import Header from './components/header/Header';
+import SideBar from './components/SideBar';
+import Main from './components/dashboard/Main';
+
+function App() {
+  return (<>
+    <Header/>
+    <SideBar/>
+    <Main/>
+  </>
+  )
+}
+
+export default App;
