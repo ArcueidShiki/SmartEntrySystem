903c865680b9f985f3ba105816202ef7dcb04734
phoever23 <gaoming93@foxmail.com>
2024-09-30 17:24:49 +0800

    change login in to administer

--- cli/src/components/header/Login.jsx
+++ cli/src/components/header/Login.jsx
@@ -6,7 +6,7 @@ function Login() {
     <nav className='header-nav ms-auto d-flex align-items-center'>
             {/* user login in */}
             <i className="bi bi-person-fill-gear icon"></i>
-            <span>Login in</span>     
+            <span>Administer</span>     
     </nav>
   )
 }
