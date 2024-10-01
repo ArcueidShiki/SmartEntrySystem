8eaa1b44dd9a513173eb29d0fc383d5dbd91e340
ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>
2024-10-01 19:52:59 +0800

    Assemble Raspberry pi components
    Signed-off-by: ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>

--- rasp/src/app.py
+++ rasp/src/app.py
@@ -1,41 +1,49 @@
 #!/usr/bin/python3
 
-from flask import Flask, Response, jsonify, request
 import io
-import logging
-from threading import Condition, Thread
-from http import server
-import smbus
 import time
+import smbus
+import logging
 import RPi.GPIO as GPIO
+from http import server
+from RPLCD.gpio import CharLCD
 from picamera2 import Picamera2
-from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
+from threading import Condition, Thread
+from picamera2.encoders import JpegEncoder
+from flask import Flask, Response, jsonify, requests
 
 app = Flask(__name__)
 
 # Servo motor configuration
 GPIO.setmode(GPIO.BCM)
-SERVO_PIN = 18
+SERVO_PIN = 21
 GPIO.setup(SERVO_PIN, GPIO.OUT)
 PWM = GPIO.PWM(SERVO_PIN, 50)
 PWM.start(0)
+OPEN = 0
+CLOSE = 1
 
 # Temperature sensor configuration
 BUS = smbus.SMBus(1)
 mlx99614_address = 0x5A
 
+# LCD configuration
+lcd = CharLCD(cols = 16,
+              rows = 2,
+              pin_rs = 7,
+              pin_e = 8,
+              pins_data = [25, 24, 23, 18],
+              numbering_mode = GPIO.BCM)
+DEFAULT_MSG = "Please wear mask and scan your temperature."
+
+def display_message(msg):
+    lcd.clear()
+    lcd.write_string(msg)
+
 def read_temperature():
     temp = BUS.read_word_data(mlx99614_address, 0x07)
     return temp * 0.02 - 273.15
-
-def set_angle(angle):
-    duty = angle / 18 + 2
-    GPIO.output(SERVO_PIN, True)
-    PWM.ChangeDutyCycle(duty)
-    time.sleep(1)
-    GPIO.output(SERVO_PIN, False)
-    PWM.ChangeDutyCycle(0)
 
 @app.route('/temp', methods=['GET'])
 def get_temp():
@@ -50,7 +58,6 @@ class StreamingOutput(io.BufferedIOBase):
         with self.condition:
             self.frame = buf
             self.condition.notify_all()
-
 
 output = StreamingOutput()
 
@@ -85,10 +92,41 @@ def start_camera():
 camera_thread = Thread(target = start_camera)
 camera_thread.start()
 
-def control_servo():
-    # 1 OPEN, 0 CLOSE
-    angle = int(request.args.get('gate_', 0))
-    # TODO  request the result from backend
+def set_angle(angle):
+    duty = angle / 18 + 2
+    GPIO.output(SERVO_PIN, True)
+    PWM.ChangeDutyCycle(duty)
+    time.sleep(1)
+    GPIO.output(SERVO_PIN, False)
+    PWM.ChangeDutyCycle(0)
+
+def open_gate():
+    set_angle(0)
+    time.sleep(3)
+    set_angle(90)
+
+def get_result():
+    RESULT_API = "http://172.20.10.11:5000/result"
+    while True:
+        try:
+            response = requests.get(RESULT_API)
+            if response.status_code == 200:
+                data = response.json()
+                result = data.get('result', CLOSE)
+                msg = data.get('message', DEFAULT_MSG)
+                if result == OPEN:
+                    open_gate(result)
+                # if not OPEN, remain close.
+                display_message(msg)
+            else:
+                print(f"Failed to fetch result, status code: {response.status_code}")
+        except Exception as e:
+            print(f"Error fetching gate control result: {e}")
+        time.sleep(5)
+
+control_thread = Thread(target = get_result)
+control_thread.daemon = True
+control_thread.start()
 
 if __name__ == '__main__':
     try:
